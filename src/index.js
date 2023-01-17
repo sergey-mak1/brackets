0916172914cc3188ae5024293e6c312d2b43de2b
sergey-mak1 <sega013021990@gmail.com>
2023-01-17 21:00:25 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+ if (str.length %2 === 0) {
+        str = Array.from(str);
+        let stack = [];
+        let x;
+
+        let brackets = Array.from(bracketsConfig.join('').replace(/,/g, ''));
+        for (elem of str) {
+            let index = brackets.indexOf(elem);
+            if (index % 2 === 0) {
+                if (elem === brackets[index + 1] && stack[stack.length - 1] === index) {
+                    stack.pop();
+                }
+                else {
+                    stack.push(index);
+                }
+            }
+            else {
+                x = stack.pop()
+                if (x !== index - 1) {
+                    return false;
+                }
+            }
+        }
+        return stack.length === 0;
+    }
+   else {
+    return false;
+   }
+        }
